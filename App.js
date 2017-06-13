111104931cec3a867d01ff379bdca32a238cc09c
dmhacker <dmhacker@yahoo.com>
2017-06-13 11:02:05 -0700

    Install fonts

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { AppRegistry, ScrollView, Image, Text, StyleSheet, View } from 'react-native'
+import { Font } from 'expo'
 
 export default class Login extends Component {
+  state = {
+    fontsLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+      'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+    this.setState({ fontsLoaded: true });
+  }
   render() {
       return (
         <View style={{flex: 1}}>
           <View style={styles.header}>
-            <Text style={{fontFamily:'OpenSans-Light', fontSize:36}}>Create an Account</Text>
+          {
+            this.state.fontsLoaded ? <Text style={{fontFamily:'OpenSans-Regular', fontSize:36}}>Create an Account</Text> : null
+          }
           </View>
           <ScrollView style={styles.login}>
             <Text style={{fontSize:96}}>Scroll me plz</Text>
@@ -19,6 +33,7 @@ export default class Login extends Component {
     );
   }
 }
+
 const styles = StyleSheet.create({
   login: {
       backgroundColor: '#efefef',
