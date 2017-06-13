90cfb3eb8355ed6561f4154d7d6e6496c2d2d1c4
Mitchell <mitchell4408@hotmail.com>
2017-06-13 13:04:18 -0700

    login page cont.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { Font } from 'expo';
 
 export default class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'OpenSans-Regular': require('../../public/fonts/OpenSans-Regular.ttf'),
+      'OpenSans-Light': require('../../public/fonts/OpenSans-Light.ttf'),
+    });
+    this.setState({
+      fontsLoaded: true
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
         <TextInput
+          placeholder="Username"
           style={styles.input}
         />
         <TextInput
+          placeholder="Password"
+          secureTextEntry
           style={styles.input}
         />
+        <TouchableOpacity style={styles.loginContainer}>
+        {
+          this.state.fontsLoaded ? <Text style={styles.loginText}>LOG IN</Text> : undefined
+        }
+        </TouchableOpacity>
       </View>
     );
   }
@@ -24,6 +48,18 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: '#FFF',
     marginBottom: 20,
-    padding: 10
+    paddingHorizontal: 10,
+    opacity: 0.9,
+    fontSize: 20
+  },
+  loginContainer: {
+    paddingVertical: 14,
+    backgroundColor: '#509E2f'
+  },
+  loginText: {
+    fontSize: 20,
+    fontFamily: 'OpenSans-Regular',
+    textAlign: 'center',
+    color:'#FFF'
   }
 });
