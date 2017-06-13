b0d8e62990a565e9f9006a6ff1c1b202c5ef5da2
dmhacker <dmhacker@yahoo.com>
2017-06-13 15:58:41 -0700

    Fix boilerplate

--- src/components/Boilerplate.js
+++ src/components/Boilerplate.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-export default class LoginForm extends Component {
+export default class Boilerplate extends Component {
   render() {
     return (
       <View style={styles.container}>
