9b7acbc2f17cd532fb34b2e4f82824d9a3147ef5
Mitchell <mitchell4408@hotmail.com>
2017-06-13 12:33:31 -0700

    Started Login Page

---
+++ src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+        />
+        <TextInput
+          style={styles.input}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#FFF',
+    marginBottom: 20,
+    padding: 10
+  }
+});
