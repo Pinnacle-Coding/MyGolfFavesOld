721e5423472cf8d718262d8560fabe50ac738a20
dmhacker <dmhacker@yahoo.com>
2017-06-14 00:05:33 -0700

    Remove LoginForm and merge Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, Image, Text, StyleSheet, View, Platform } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text, KeyboardAvoidingView, Linking } from 'react-native';
 import { Font } from 'expo';
-import LoginForm from './LoginForm';
+import { Link } from 'react-router-native'
+
+import focusTextInput from '../utils/TextInputManager.js'
+import renderIf from '../utils/renderif.js'
 
 export default class Login extends Component {
   constructor() {
@@ -9,6 +12,12 @@ export default class Login extends Component {
     this.state = {
       fontsLoaded: false
     };
+  }
+
+  toggleStatus() {
+    this.setState({
+      showForgotLogin:!this.state.showForgotLogin
+    });
   }
 
   async componentDidMount() {
@@ -28,20 +37,97 @@ export default class Login extends Component {
             this.state.fontsLoaded ? <Text style={textStyles.titleText}>Login</Text> : undefined
           }
           </View>
-          <View style={localStyles.container}>
-            <LoginForm />
-          </View>
-        </View>
+          <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <View flexDirection="row" style={styles.topText}>
+              {this.state.fontsLoaded ? <Text style={styles.noAccountTxt}>Dont have an account? </Text> : undefined }
+              <Link to="/register">
+                {this.state.fontsLoaded ? <Text style={styles.createAccntLink}>Create an Account</Text> : <Text>Create an Account</Text> }
+              </Link>
+            </View>
+
+            <TextInput
+              placeholder="Username"
+              returnKeyType="next"
+              onSubmitEditing={() => focusTextInput(this.refs.passwordInput)}
+              style={styles.input}
+            />
+
+            <TextInput
+              placeholder="Password"
+              secureTextEntry
+              returnKeyType="go"
+              style={styles.input}
+              ref='passwordInput'
+            />
+
+            <TouchableOpacity onPress={() => this.toggleStatus() }>
+               {this.state.fontsLoaded ? <Text style={styles.forgot}>Forgot Username/Password</Text> : undefined }
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.loginContainer}>
+              { this.state.fontsLoaded ? <Text style={styles.loginText}>LOG IN</Text> : undefined }
+            </TouchableOpacity>
+
+            {renderIf(this.state.showForgotLogin)(
+              <View>
+                <TextInput
+                  placeholder="Email Address"
+                  returnKeyType="go"
+                  style={styles.input}
+                />
+
+                <TouchableOpacity style={styles.loginContainer}>
+                  { this.state.fontsLoaded ? <Text style={styles.loginText}>SEND LOGIN CREDENTIALS</Text> : undefined }
+                </TouchableOpacity>
+              </View>
+            )}
+
+          </KeyboardAvoidingView>
+      </View>
     );
   }
 }
 
 import textStyles from '../styles/text.js';
 import containerStyles from '../styles/containers.js';
-const localStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     backgroundColor: '#efefef',
     padding: 30,
     flex: 1
+  },
+  noAccountTxt: {
+    fontSize: 17,
+    fontFamily: 'OpenSans-Regular'
+  },
+  createAccntLink: {
+    color: '#509E2f',
+    fontSize: 17,
+    fontFamily: 'OpenSans-Regular'
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#FFF',
+    marginTop: 20,
+    paddingHorizontal: 10,
+    opacity: 0.9,
+    fontSize: 20
+  },
+  loginContainer: {
+    marginTop: 20,
+    paddingVertical: 14,
+    backgroundColor: '#509E2f'
+  },
+  forgot: {
+    color: '#509E2f',
+    fontSize: 17,
+    marginTop: 20,
+    fontFamily: 'OpenSans-Regular'
+  },
+  loginText: {
+    fontSize: 20,
+    fontFamily: 'OpenSans-Regular',
+    textAlign: 'center',
+    color:'#FFF'
   }
 });
