e1a0350b478aa06d2ae00601f5c4e9422ffe8750
Mitchell <mitchell4408@hotmail.com>
2017-06-13 17:28:05 -0700

    app.js stupid

--- App.js
+++ App.js
@@ -1,4 +1,22 @@
-import Register from './src/components/Register.js'
-import Login from './src/components/Login.js'
+import React, { Component } from 'react';
+import { StyleSheet, View, AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Register from './src/components/Register.js';
+import Login from './src/components/Login.js';
 
-module.exports = Register;
+export default class App extends Component {
+  static navigationOptions = {
+
+  }
+  render() {
+    return (
+      <Login/>
+    );
+  }
+}
+
+const Test = StackNavigator({
+  Home: { screen: App },
+  Login: { screen: Login },
+  Register: { screen: Register}
+});
