e89925d7235912ded2dda6d25716f811cd86d58e
dmhacker <dmhacker@yahoo.com>
2017-06-13 18:40:48 -0700

    New stuffz

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Register from './src/components/Register.js';
 import Login from './src/components/Login.js';
+import Home from './src/components/Home.js';
 
 export default class App extends Component {
   static navigationOptions = {
@@ -10,7 +11,7 @@ export default class App extends Component {
   }
   render() {
     return (
-      <Login/>
+      <Home/>
     );
   }
 }
