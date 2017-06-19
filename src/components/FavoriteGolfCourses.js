98ba3a44e2093723052810e445dfebf95842647b
Mitchell Sayer <mitchellsayer@Mitchells-MBP.home>
2017-06-18 19:36:00 -0700

    started custom list

--- src/components/FavoriteGolfCourses.js
+++ src/components/FavoriteGolfCourses.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { StyleSheet, View,  } from 'react-native';
+import { StyleSheet, View, ListView, Text, TouchableOpacity  } from 'react-native';
 import { Font } from 'expo';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { List } from 'react-native-elements';
+import { CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from './Header.js'
+
+var testData = [
+  {
+    name: 'Agoura Hills Country Club',
+    distance: '7.67',
+    checked: true,
+    link: 'http://www.google.com'
+  },
+  {
+    name: 'Moorpark Country Club',
+    distance: '10.19',
+    checked: false,
+    link: 'http://www.yahoo.com'
+  }
+]
 
 export default class Boilerplate extends Component {
   constructor() {
     super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => ri != r2})
+
     this.state = {
-      fontsLoaded: false
+      fontsLoaded: false,
+      dataSource: ds.cloneWithRows(testData),
+      checkedSrc: 'check-box'
     };
   }
+
   async componentDidMount() {
     await Font.loadAsync({
       'OpenSans-Regular': require('../../assets/fonts/OpenSans-Regular.ttf'),
@@ -22,26 +43,40 @@ export default class Boilerplate extends Component {
       fontsLoaded: true
     })
   }
+
+  changeChecked() {
+    if (this.state.checkedSrc.equals('check-box')){
+      this.setState({checkedSrc: 'check-box-outline-blank'})
+    } else {
+      this.setState({checkedSrc: 'check-box'})
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header title="Favorite Golf Courses"/>
         <View style={{borderBottomColor:'gray', borderBottomWidth:1, borderStyle: 'solid', padding:0}}/>
-        <KeyboardAwareScrollView
-          style={styles.scrollContainer}
-          resetScrollToCoords={{ x: 0, y: 0 }}
-          extraHeight={175}
-          keyboardOpeningTime={0}
-          scrollEnabled={true}>
-          <View style={styles.paramContainer}>
-
-          </View>
-          <View >
-
-          </View>
-        </KeyboardAwareScrollView>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+            <View style = {{flexDirection: 'row'}}>
+              <TouchableOpacity onPress={() => {
+                rowData.checked = !rowData.checked;
+                this.changeChecked();
+              }}>
+                <Icon
+                  name={this.state.checkedSrc}
+                  size={30}
+                  style={{padding: 10}}
+                />
+              </TouchableOpacity>
+              <Text style={{fontSize: 20, alignSelf:'center'}}>{rowData.name}</Text>
+              <Text style={{fontSize: 15, alignSelf:'center'}}>{rowData.distance} mi.</Text>
+            </View>}
+          />
       </View>
-    );
+    )
   }
 }
 
@@ -53,7 +88,7 @@ const styles = StyleSheet.create({
 
   },
   paramContainer: {
-    padding: 20
+
   },
 
 });
