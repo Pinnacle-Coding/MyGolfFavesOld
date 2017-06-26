b1710170f441c6629ce89651063e611b93819d37
dmhacker <dmhacker@yahoo.com>
2017-06-25 21:33:21 -0700

    secure text entry

--- src/components/Redeem.js
+++ src/components/Redeem.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Font } from 'expo';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import Header from './Header.js'
+
+var t = require('tcomb-form-native');
+var Form = t.form.Form;
+var RedeemModel = t.struct({
+  redemptionCode: t.String,
+  enterAmount: t.Number
+});
+var options = {
+  fields: {
+    redemptionCode: {
+      secureTextEntry: true
+    }
+  }
+};
 
 var offer = {
   key: 2058,
@@ -31,6 +46,19 @@ export default class Boilerplate extends Component {
       <View style={styles.container}>
         <Header title="Redeem Offer"/>
         <View style={{borderBottomColor:'gray', borderBottomWidth:1, borderStyle: 'solid', padding:0}}/>
+        <KeyboardAwareScrollView
+        style={styles.scrollContainer}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        extraHeight={175}
+        keyboardOpeningTime={0}
+        scrollEnabled={true}>
+          <Form
+            ref="form"
+            type={RedeemModel}
+            options={options}
+          />
+          <View style={{padding:100}}/>
+      </KeyboardAwareScrollView>
       </View>
     );
   }
@@ -39,5 +67,8 @@ export default class Boilerplate extends Component {
 const styles = StyleSheet.create({
   container: {
 
+  },
+  scrollContainer: {
+    padding: 20
   }
 });
