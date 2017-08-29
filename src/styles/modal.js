caac86b3699147f06b142b87034c8bc098a31e79
dmhacker <dmhacker@yahoo.com>
2017-08-28 18:27:43 -0700

    Modal styles

---
+++ src/styles/modal.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  modalCloseButton: {
+    backgroundColor: '#509E2f',
+    padding: 12,
+    margin: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  }
+});
+
+export default styles
